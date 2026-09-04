5604fed13d0f26818277e2d9fcfd91f618a0e8de
marvelyusuf44-dotcom <marvelyusuf44@gmail.com>
2026-09-04 10:28:34 +0000

    fix kedua index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 export default async function handler(req: any, res: any) {
+  // @ts-ignore
   const { default: app } = await import("../artifacts/api-server/dist/app.mjs");
   return app(req, res);
 }
